62f0247f176abc21c8ac601694f1f7fb6b9b2fb3
segun <soshice@gmail.com>
2023-04-20 10:59:12 +0100

    css file disconnected

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import "./app.css"
 import Topinfo from "./components/TopInfo/Topinfo";
 import About from "./components/About/About";
 import Tools from "./components/Tools/Tools";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Intro from "./components/Intro/Intro";
-
 
 function App() {
   return <div className="bg-black text-white min-w-[200px] overflow-x-hidden">
